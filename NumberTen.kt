7553c46033aaf168c7bfd4c78c484391f49d3fe2
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-11-05 06:56:59 +0700

    number 10 done

--- NumberTen.kt
+++ NumberTen.kt
@@ -1,4 +1,9 @@
 fun main (){
     val numbers = listOf(3, 1, 7, 4, 5, 6, 8, 2)
     val indices = listOf(8, 7, 2, 8, 2, 6)
+    for (i in numbers){
+        if (indices.contains(i)){
+            print(i)
+        }
+    }
 }
